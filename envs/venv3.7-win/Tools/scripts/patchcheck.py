c4be18a09577b4f4d5c6c1d692ae274851b6ea9d
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-22 16:13:22 +0300

    Migrate from master/slave vocabulary

--- envs/venv3.7-win/Tools/scripts/patchcheck.py
+++ envs/venv3.7-win/Tools/scripts/patchcheck.py
@@ -75,7 +75,7 @@ def get_base_branch():
         return None
     version = sys.version_info
     if version.releaselevel == 'alpha':
-        base_branch = "master"
+        base_branch = "main"
     else:
         base_branch = "{0.major}.{0.minor}".format(version)
     this_branch = get_git_branch()
